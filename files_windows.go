bd72cc10305d1d4483bd4d37b98edb3c23b3966a
Nex <nex@nex.sx>
2018-01-12 13:19:05 +0100

    Better handling

--- files_windows.go
+++ files_windows.go
@@ -7,6 +7,12 @@ import (
 func ExpandWindows(path string) string {
 	re := regexp.MustCompile(`\%(?i)(\w*)\%`)
 	match := re.FindStringSubmatch(path)
+	if match == nil {
+		return path
+	}
 	newValue := os.Getenv(match[1])
+	if newValue == "" {
+		return path
+	}
 	return re.ReplaceAllString(path, newValue)
 }
