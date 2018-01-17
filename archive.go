2e4e5086694847032acea9b56c09d69c2dbeb4e9
Nex <nex@nex.sx>
2018-01-17 13:51:01 +0100

    Added archive function

---
+++ archive.go
@@ -0,0 +1,73 @@
+package files
+
+// Taken from:
+// https://gist.github.com/svett/424e6784facc0ba907ae
+
+import (
+	"os"
+	"io"
+	"strings"
+	"archive/zip"
+	"path/filepath"
+)
+
+func Zip(source, destination string) error {
+	zipFile, err := os.Create(destination)
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
+
+	archive := zip.NewWriter(zipFile)
+	defer archive.Close()
+
+	info, err := os.Stat(source)
+	if err != nil {
+		return nil
+	}
+
+	var baseDir string
+	if info.IsDir() {
+		baseDir = filepath.Base(source)
+	}
+
+	filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		if baseDir != "" {
+			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
+		}
+
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			header.Method = zip.Deflate
+		}
+
+		writer, err := archive.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		_, err = io.Copy(writer, file)
+		return err
+	})
+
+	return err
+}
